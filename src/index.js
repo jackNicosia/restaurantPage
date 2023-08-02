d2d75632ac0624ba8a99a3a1e6d44791516a0fea
Jack <jackrileynicosia@yahoo.com>
2023-08-02 00:22:18 -0400

    page is made with javascript now

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './imgs/food.png';
+import myName from './pageLoad';
+import navHeader from './pageLoad';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
-
-    //Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-   /* const myPicture = new Image();
-    myPicture.src = 
-*/
-
-
 
     return element;
   }
   
   document.body.appendChild(component());
+
+
+
+
+   
